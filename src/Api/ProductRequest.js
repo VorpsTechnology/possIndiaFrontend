967e4b254f465bf6e36ad3033da254dfb4ba6f59
Adarsh Raj <ahazadarsh0014@gmail.com>
2023-05-04 07:02:49 +0530

    first commit 4

--- src/Api/ProductRequest.js
+++ src/Api/ProductRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//const API=axios.create({baseURL:"https://server.possindia.in"})
-const API=axios.create({baseURL:"http://localhost:5005"})
+const API=axios.create({baseURL:"https://server.possindia.in"})
+//const API=axios.create({baseURL:"http://localhost:5005"})
 export const getAllProducts=(data)=>API.post('/product/getproducts',data)
 export const getProduct=(id)=>API.get(`/product/${id}`)
 export const deleteProduct=(id)=>API.delete(`/product/${id}`)
